28b41d505bda6751459114ac381e0dc72830fa77
zhenquan <1140377034@qq.com>
2016-12-22 23:59:03 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/example/lizhenquan/coolweather/util/Utility.java
+++ app/src/main/java/com/example/lizhenquan/coolweather/util/Utility.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 import com.example.lizhenquan.coolweather.db.City;
 import com.example.lizhenquan.coolweather.db.County;
 import com.example.lizhenquan.coolweather.db.Province;
+import com.example.lizhenquan.coolweather.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -79,5 +81,22 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    /**
+     * 将返回的JSON数据解析成Weather实体类
+     * @param response
+     * @return
+     */
+    public static Weather handleWeatherResponse(String response){
+        try{
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return  new Gson().fromJson(weatherContent,Weather.class);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return  null;
     }
 }
