855b87b4c3deed3470bcca8207f63d5d46c13866
zhenquan <1140377034@qq.com>
2017-02-11 14:28:20 +0800

    保存学习项目

--- app/src/main/java/com/example/lizhenquan/coolweather/ChooseAreaFragment.java
+++ app/src/main/java/com/example/lizhenquan/coolweather/ChooseAreaFragment.java
@@ -97,14 +97,14 @@ public class ChooseAreaFragment extends Fragment {
                 } else if (currentLevel == LEVEL_CITY) {
                     selectedCity = cityList.get(position);
                     queryCounties();
-                }else if (currentLevel == LEVEL_COUNTY){
+                } else if (currentLevel == LEVEL_COUNTY) {
                     String weatherId = countyList.get(position).getWeatherId();
-                    if (getActivity() instanceof  MainActivity) {
+                    if (getActivity() instanceof MainActivity) {
                         Intent intent = new Intent(getActivity(), WeatherActivity.class);
                         intent.putExtra("weather_id", weatherId);
                         startActivity(intent);
                         getActivity().finish();
-                    }else if (getActivity() instanceof  WeatherActivity){
+                    } else if (getActivity() instanceof WeatherActivity) {
                         WeatherActivity activity = (WeatherActivity) getActivity();
                         activity.drawerLayout.closeDrawers();
                         activity.swipeRefresh.setRefreshing(true);
